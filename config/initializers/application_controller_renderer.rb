2e1a72100313e260fc678371685d4e0cd26b03af
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-05-09 10:09:43 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '63310ad0492b0abe55f8b0ae9295cd486ba82e51bfbecd9eeffffdbcc344788b22c38b293681d7b6b0d8f042a4b9ce231f8daa91441ca29bb10a787fc0278b6d'
